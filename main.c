88bf55f9be78006cf49cd90366c0e9f021d945ef
William Gregorio <mrwilliamgregorio@yahoo.com>
2025-04-04 20:41:39 -0400

    sigmoid fuctions and weight init prepping

--- main.c
+++ main.c
@@ -5,7 +5,20 @@
 #define inputs 2
 #define hidden_nodes 2
 #define outputs 1
-#define training_sets 6
+#define training_sets 4
+
+// 0 and 1 for weight initialization
+double weights() {
+  return ((double)rand()) / ((double)RAND_MAX);
+}
+
+double sigmoid_activation(double x) {
+  return 1 / (1 + exp(-x));
+}
+
+double derivative_sigmoid(double x) {
+  return x * (1 - x);
+}
 
 int main(void) {
   const double learning_rate =  0.1f;
@@ -19,4 +32,6 @@ int main(void) {
   double hidden_weights[inputs][hidden_nodes];
   double output_weights[hidden_nodes][ouputs];
 
+  double training_inputs[training_sets][inputs] = {{0.0f, 0.0f}, {1.0f, 0.0f}, {0.0f, 1.0f}, {1.0f, 1.0f}};
+  double training_outputs[training_sets][outputs] = {{0.0f, 0.0f}, {1.0f, 0.0f}, {0.0f, 1.0f}, {1.0f, 1.0f}};
 }
