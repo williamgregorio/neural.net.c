0eef2e1c307908640723111f496a94ecbdf00735
William Gregorio <mrwilliamgregorio@yahoo.com>
2025-04-04 20:28:22 -0400

    defined layers, weights, lr, hidd nodes

--- main.c
+++ main.c
@@ -6,3 +6,17 @@
 #define hidden_nodes 2
 #define outputs 1
 #define training_sets 6
+
+int main(void) {
+  const double learning_rate =  0.1f;
+
+  double hidden_layer[hidden_nodes];
+  double hidden_layer_bias[hidden_nodes];
+
+  double output_layer[outputs];
+  double output_layer_bias[outputs];
+
+  double hidden_weights[inputs][hidden_nodes];
+  double output_weights[hidden_nodes][ouputs];
+
+}
